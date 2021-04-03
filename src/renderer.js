0d89a4d9d9d05dd033022f8c8a79f0bab6902bd0
Vladislav <83463425639@mail.ru>
2021-04-03 15:17:57 +0500

    Connect bd

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,48 @@
-function celciusToFahrenheit(){
-    let celcius = document.getElementById('celcius').value;
-    let fahrenheit = (celcius* 9/5) + 32;
-    document.getElementById('fahrenheit').value = fahrenheit;
-
+function log(){
+    let login = document.getElementById('login').value;
+    let password = document.getElementById('password').value;
+    if (login=="admin" && password=="5677"){
+        document.location.href='menu.html';
+    }
+    else
+    {
+        document.location.href='index.html';
+    }
+}
+const sqlite = require('sqlite3');
+//const db = new sqlite.Database('./public/db.sqlite3', (err) => {
+//    if (err) console.error('Database opening error: ', err);
+//});
+//db.run()
+// let db = new sqlite3.Database('avto');
+async function main () {
+    console.log('Start');
+    let db = await new Promise((resolve, rejectc) =>{
+        let db = new sqlite.Database('avto', (err) => {
+            if (err) {
+              return rejectc(err.message);
+            }
+            console.log('Connected to the in-memory SQlite database.');
+            resolve(db);
+            
+        })
+    });
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+      console.log('finish cmd');
+    };
+    
+async function main2 () {
+    console.log('start');
+    let db = new sqlite.Database('avto');
+    console.log('Connected to the in-memory SQlite database.');
+    db.close();
+    console.log('end')
+    
 }
 
-function fahrenheitToCelcius(){
-    let fahrenheit = document.getElementById('fahrenheit').value;
-    let celcius = (fahrenheit - 32) * 5/9
-    document.getElementById('celcius').value = celcius;
-}
+main2();
